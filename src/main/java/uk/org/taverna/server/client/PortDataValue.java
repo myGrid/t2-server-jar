94878055d8b1ba6378e913e37db600d4bfc287c1
Robert Haines <rhaines@manchester.ac.uk>
2013-05-20 14:40:15 +0100

    Stop caching data client-side.
    This is not the responsibility of this library. Anyone using this library is
    welcome to cache their own data if they wish.

--- src/main/java/uk/org/taverna/server/client/PortDataValue.java
+++ src/main/java/uk/org/taverna/server/client/PortDataValue.java
@@ -35,11 +35,8 @@ package uk.org.taverna.server.client;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.ref.SoftReference;
 import java.net.URI;
-import java.util.Arrays;
 
-import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.math.LongRange;
 
 import uk.org.taverna.server.client.util.IOUtils;
@@ -53,15 +50,8 @@ public class PortDataValue extends AbstractPortValue {
 	// If there is no data...
 	private static final byte[] EMPTY_DATA = new byte[0];
 
-	// Data cache
-	private SoftReference<byte[]> cache;
-	private LongRange dataGot;
-
 	PortDataValue(Run run, URI reference, String type, long size) {
 		super(run, reference, type, size);
-
-		cache = new SoftReference<byte[]>(null);
-		dataGot = null;
 	}
 
 	@Override
@@ -145,11 +135,6 @@ public class PortDataValue extends AbstractPortValue {
 
 	private byte[] getData(LongRange range) {
 
-		byte[] data = cache.get();
-		if (data == null) {
-			dataGot = null;
-		}
-
 		// Return empty data if this value is empty.
 		if (getDataSize() == 0
 				|| contentType.equalsIgnoreCase("application/x-empty")) {
@@ -166,101 +151,6 @@ public class PortDataValue extends AbstractPortValue {
 					(getDataSize() - 1));
 		}
 
-		// Find the data range(s) that we need to download.
-		LongRange[] need = fill(dataGot, range);
-
-		switch (need.length) {
-		case 0:
-			// We already have all the data we need, just return the right bit.
-			// dataGot cannot be null here and must fully encompass range.
-			int from = (int) (range.getMinimumLong() - dataGot.getMinimumLong());
-			int to = (int) (range.getMaximumLong() - dataGot.getMinimumLong());
-
-			// copyOfRange is exclusive!
-			return Arrays.copyOfRange(data, from, (to + 1));
-		case 1:
-			// we either have some data, at one end of range or either side of
-			// it, or none. dataGot can be null here.
-			// In both cases we download what we need.
-			byte[] newData = run.getOutputData(reference, need[0]);
-			if (dataGot == null) {
-				// This is the only data we have, return it all.
-				dataGot = range;
-				data = newData;
-				cache = new SoftReference<byte[]>(data);
-				return data;
-			} else {
-				// Add the new data to the correct end of the data we have,
-				// then return the range requested.
-				if (range.getMaximumLong() <= dataGot.getMaximumLong()) {
-					dataGot = new LongRange(range.getMinimumLong(),
-							dataGot.getMaximumLong());
-					data = ArrayUtils.addAll(newData, data);
-					cache = new SoftReference<byte[]>(data);
-
-					// copyOfRange is exclusive!
-					return Arrays.copyOfRange(data, 0,
-							(int) (range.getMaximumLong() + 1));
-				} else {
-					dataGot = new LongRange(dataGot.getMinimumLong(),
-							range.getMaximumLong());
-					data = ArrayUtils.addAll(data, newData);
-					cache = new SoftReference<byte[]>(data);
-
-					// copyOfRange is exclusive!
-					return Arrays.copyOfRange(data, (int) (range
-							.getMinimumLong() - dataGot.getMinimumLong()),
-							(int) (dataGot.getMaximumLong() + 1));
-				}
-			}
-		case 2:
-			// We definitely have some data and it is in the middle of the
-			// range requested. dataGot cannot be null here.
-			dataGot = range;
-			byte[] data1 = run.getOutputData(reference, need[0]);
-			byte[] data2 = run.getOutputData(reference, need[1]);
-			data = ArrayUtils.addAll(data1, data);
-			data = ArrayUtils.addAll(data, data2);
-			cache = new SoftReference<byte[]>(data);
-			return data;
-		}
-
-		// Should never get here! This is an error!
-		return null;
-	}
-
-	// Aaaarrrgh!
-	private LongRange[] fill(LongRange got, LongRange want) {
-		if (got == null) {
-			return new LongRange[] { want };
-		}
-
-		if (got.containsLong(want.getMinimumLong())) {
-			if (got.containsLong(want.getMaximumLong())) {
-				return new LongRange[0];
-			} else {
-				return new LongRange[] { new LongRange(
-						(got.getMaximumLong() + 1), want.getMaximumLong()) };
-			}
-		} else {
-			if (got.containsLong(want.getMaximumLong())) {
-				return new LongRange[] { new LongRange(want.getMinimumLong(),
-						(got.getMinimumLong() - 1)) };
-			} else {
-				if (want.getMaximumLong() < got.getMinimumLong()) {
-					return new LongRange[] { new LongRange(
-							want.getMinimumLong(), (got.getMinimumLong() - 1)) };
-				} else if (want.getMinimumLong() > got.getMaximumLong()) {
-					return new LongRange[] { new LongRange(
-							(got.getMaximumLong() + 1), want.getMaximumLong()) };
-				} else {
-					return new LongRange[] {
-							new LongRange(want.getMinimumLong(),
-									(got.getMinimumLong() - 1)),
-									new LongRange((got.getMaximumLong() + 1),
-											want.getMaximumLong()) };
-				}
-			}
-		}
+		return run.getOutputData(reference, range);
 	}
 }
